63690807ad2e144faca98f23752d5c5ef9f8e8d2
Aleksandr Rogozhkin <rogozkin371@gmail.com>
2023-05-01 20:40:46 +0300

    jj

---
+++ src/main/java/org/example/SumCalculator.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class SumCalculator {
+    public int sum(int n) {
+        if(n<=0){
+            throw new IllegalArgumentException("Не може бути нуль або меньше нуля");
+        }
+        int result=0;
+        for (int i = 0; i<=n ; i++){
+            result=result+i;
+        }
+        return result;
+    }
+}
+
+
+
+
+
---
+++ src/test/java/org/example/SumCalculatorTests.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+
+class SumCalculatorTests {
+    private SumCalculator sumCalculator;
+    @BeforeEach
+    public void beforeEach(){
+        sumCalculator=new SumCalculator();
+    }
+    @Test
+    public void testSumOne(){
+        //When
+        int actual = sumCalculator.sum(1);
+
+        //Then
+        int expected = 1;
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void testSumThree(){
+        //When
+        int actual = sumCalculator.sum(3);
+
+        //Then
+        int expected = 6;
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void testThrowEx(){
+        Assertions.assertThrows(IllegalArgumentException.class,()-> sumCalculator.sum(0));
+
+        }
+}
+
